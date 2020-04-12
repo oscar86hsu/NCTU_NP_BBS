69f5aff9affe3ec0fe1d2531e3950642024bd9d5
oscar86hsu.cs05 <oscar86hsu.cs05@o365.nctu.edu.tw>
2020-04-12 12:12:42 +0000

    Add Board Test

--- test.py
+++ test.py
@@ -3,17 +3,24 @@ import unittest
 import socket
 import time
 import threading
+import logging
 import server
 from db import Database
+
+# logging.basicConfig(level=logging.DEBUG,
+#                     format='%(asctime)s [%(levelname)s] %(message)s',
+#                     datefmt='%Y-%m-%d %H:%M:%S')
 
 HOST = "127.0.0.1"
 PORT = 6000
 
+
 class ConnectionTest(unittest.TestCase):
-    @classmethod    
+    @classmethod
     def setUpClass(cls):
         test_server = server.BBS_Server(HOST, PORT, "test_connection.db")
-        t = threading.Thread(target=test_server.start_listening, args=(), daemon=True)
+        t = threading.Thread(
+            target=test_server.start_listening, args=(), daemon=True)
         t.start()
         time.sleep(0.1)
 
@@ -44,12 +51,14 @@ class ConnectionTest(unittest.TestCase):
         for c in connections:
             c.join()
 
-class BasicLoginTest(unittest.TestCase):
-    @classmethod    
+
+class LoginTest(unittest.TestCase):
+    @classmethod
     def setUpClass(cls):
         dbname = "test_login.db"
         test_server = server.BBS_Server(HOST, PORT, dbname)
-        t = threading.Thread(target=test_server.start_listening, args=(), daemon=True)
+        t = threading.Thread(
+            target=test_server.start_listening, args=(), daemon=True)
         t.start()
         time.sleep(0.1)
         db = Database(dbname)
@@ -70,7 +79,8 @@ class BasicLoginTest(unittest.TestCase):
         s.send("register\r\n".encode())
         time.sleep(0.1)
         raw_message = s.recv(1024)
-        self.assertIn(b'Usage: register <username> <email> <password>\n', raw_message)
+        self.assertIn(
+            b'Usage: register <username> <email> <password>\n', raw_message)
         s.send("exit\r\n".encode())
         s.close()
         del s
@@ -94,7 +104,7 @@ class BasicLoginTest(unittest.TestCase):
         s.send("exit\r\n".encode())
         s.close()
         del s
-        
+
     def test_login_command_fail(self):
         s = self.connect()
         s.send("login\r\n".encode())
@@ -186,6 +196,136 @@ class BasicLoginTest(unittest.TestCase):
         s.send("exit\r\n".encode())
         s.close()
         del s
+
+
+class BoardTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        dbname = "test_board.db"
+        test_server = server.BBS_Server(HOST, PORT, dbname)
+        t = threading.Thread(
+            target=test_server.start_listening, args=(), daemon=True)
+        t.start()
+        time.sleep(0.1)
+        db = Database(dbname)
+        db.create_user('exist_user', 'exist_email', 'exist_password')
+        db.create_board('exist_board', '1')
+        db.create_board('key_board', '1')
+
+    @classmethod
+    def tearDownClass(cls):
+        os.remove("test_board.db")
+
+    def connect(self):
+        s = socket.socket()
+        s.connect((HOST, PORT))
+        raw_message = s.recv(1024)
+        return s
+
+    def test_create_board_success(self):
+        s = self.connect()
+        s.send("login exist_user exist_password\r\n".encode())
+        time.sleep(0.1)
+        raw_message = s.recv(1024)
+        self.assertIn(b'Welcome, exist_user.\n', raw_message)
+
+        s.send("create-board new_board\r\n".encode())
+        time.sleep(0.1)
+        raw_message = s.recv(1024)
+        self.assertIn(b'Create board successfully.\n', raw_message)
+
+        s.send("exit\r\n".encode())
+        s.close()
+        del s
+
+        sql = ''' DELETE FROM board WHERE name="new_board" '''
+        Database("test_board.db").execute(sql)
+        
+
+    def test_create_board_fail_exist(self):
+        s = self.connect()
+        s.send("login exist_user exist_password\r\n".encode())
+        time.sleep(0.1)
+        raw_message = s.recv(1024)
+        self.assertIn(b'Welcome, exist_user.\n', raw_message)
+
+        s.send("create-board exist_board\r\n".encode())
+        time.sleep(0.1)
+        raw_message = s.recv(1024)
+        self.assertIn(b'Board is already exist.\n', raw_message)
+
+        s.send("exit\r\n".encode())
+        s.close()
+        del s
+
+    def test_create_board_fail_login(self):
+        s = self.connect()
+        s.send("create-board new_board\r\n".encode())
+        time.sleep(0.1)
+        raw_message = s.recv(1024)
+        self.assertIn(b'Please login first.\n', raw_message)
+
+        s.send("exit\r\n".encode())
+        s.close()
+        del s
+
+    def test_create_board_fail_command(self):
+        s = self.connect()
+        s.send("create-board\r\n".encode())
+        time.sleep(0.1)
+        raw_message = s.recv(1024)
+        self.assertIn(b'Usage: create-board <name>\n', raw_message)
+
+        s.send("exit\r\n".encode())
+        s.close()
+        del s
+
+    def test_list_all_board_success(self):
+        s = self.connect()
+        s.send("list-board\r\n".encode())
+        time.sleep(0.1)
+        raw_message = s.recv(1024)
+        header = "{:10}{:15}{:15}\n".format(
+            "Index", "Name", "Moderator").encode()
+        board_list = "{:10}{:15}{:15}\n".format("1", "exist_board", "exist_user").encode() + "{:10}{:15}{:15}\n".format("2", "key_board", "exist_user").encode()
+        self.assertIn(header + board_list, raw_message)
+
+        s.send("exit\r\n".encode())
+        s.close()
+        del s
+
+    def test_list_key_board_success(self):
+        s = self.connect()
+        s.send("list-board ##key\r\n".encode())
+        time.sleep(0.1)
+        raw_message = s.recv(1024)
+        header = "{:10}{:15}{:15}\n".format(
+            "Index", "Name", "Moderator").encode()
+        board_list = "{:10}{:15}{:15}\n".format(
+            "2", "key_board", "exist_user").encode()
+        self.assertIn(header + board_list, raw_message)
+
+        s.send("exit\r\n".encode())
+        s.close()
+        del s
+
+    def test_list_board_fail_command(self):
+        s = self.connect()
+        s.send("list-board key\r\n".encode())
+        time.sleep(0.1)
+        raw_message = s.recv(1024)
+        self.assertIn(b"Usage: list-board (##<key>)\n", raw_message)
+
+        s.send("list-board key is exist\r\n".encode())
+        time.sleep(0.1)
+        raw_message = s.recv(1024)
+        self.assertIn(b"Usage: list-board (##<key>)\n", raw_message)
+
+        s.send("exit\r\n".encode())
+        s.close()
+        del s
+
+
 
 
 if __name__ == "__main__":
